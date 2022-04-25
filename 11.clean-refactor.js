9627985ad738cb5b4b1b66e4fd524eb603014126
alejandrofdez-us <afdez@us.es>
2022-04-25 12:38:06 +0200

    formatting errros

--- 11.clean-refactor.js
+++ 11.clean-refactor.js
@@ -8,37 +8,42 @@ const Order = models.order
 exports.deliver = async function (req, res) {
   // the err from validation result
   const err = validationResult(req)
-  if (err.errors.length > 0) {
+  if (err.errors.length>0) {
     res.status(422).send(err)
-  } else {
-    try {
-      const O = await Order.findByPk(req.params.orderId) //the order
-      if (!O) {
-        res.status(404).send('Order not found')
-      } else {
-        O.deliveredAt = new Date()
-        //O.priority=null
-        //O.setQuality(moment(o.deliveredAt).diff(moment(o.createdAt), 'minutes')%5)
-        let uO = await O.save()
+  }
+  else {
+  try {
+    const O = await Order.findByPk(req.params.orderId) //the order
+    if (!O) {
+      res.status(404).send("Order not found")
+    }
+    else {
+      O.deliveredAt=new Date()
+      //O.priority=null
+      //O.setQuality(moment(o.deliveredAt).diff(moment(o.createdAt), 'minutes')%5)
+      let uO = await O.save()
 
-        const current_res_data = await Restaurant.findByPk(O.restaurantId)
+      const current_res_data = await Restaurant.findByPk(O.restaurantId)
 
-        // compute the average service time for current restaurant
-        // retrieve orders
-        const orders = await current_res_data.getOrders()
-        const times_computed = orders.filter(o => o.deliveredAt).map(o => moment(o.deliveredAt).diff(moment(o.createdAt), 'minutes'))
-        const tm = times_computed.reduce((acc, serviceTime) => acc + serviceTime, 0) / times_computed.length
+      // compute the average service time for current restaurant
+      // retrieve orders
+      const orders = await current_res_data.getOrders()
+      const times_computed = orders.filter(o => o.deliveredAt).map(o => moment(o.deliveredAt).diff(moment(o.createdAt), 'minutes'))
+      const tm = times_computed.reduce((acc, serviceTime) => acc + serviceTime, 0) / times_computed.length
 
-        await Restaurant.update({ averageServiceMinutes: tm }, { where: { id: O.restaurantId } })
-        uO = await O.reload()
-        res.json(uO)
-      }
+      await Restaurant.update({  averageServiceMinutes: tm }, { where: { id: O.restaurantId } })
+          uO = await O.reload()
+      res.json(uO)
+    }
+
+
     } catch (err) {
       if (err.name.includes('ValidationError')) {
-        res.status(422).send(err)
+      res.status(422).send(err)
       } else {
+
         res.status(500).send(err)
-      }
-    }
-  }
+}
+}
+}
 }
